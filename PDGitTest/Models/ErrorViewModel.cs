0132fcec1813fb6c43734aa06321099422f3ea26
Phi Duong <PDuong@dhs.lacounty.gov>
2021-09-15 15:02:44 -0700

    Init

---
+++ PDGitTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDGitTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
